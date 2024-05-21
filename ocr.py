73ac3436a7f1cad56058ba1e7c51849a2e9b68ca
Siddharth Panditrao <115323514+SoloSid69@users.noreply.github.com>
2024-05-21 12:48:53 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -30,27 +30,44 @@ def extract_number_from_image(roi):
     else:
         return 0
 
+def verify_number_in_image(roi, target_number):
+    config = '--psm 10 --oem 3 -c tessedit_char_whitelist=0123456789'
+    number = pytesseract.image_to_string(roi, config=config)
+    
+    if '9' in number.split():
+        return 9
+    else:
+        return 0
+    
 def process(cells, img):
     sudoku_array = [[0 for _ in range(9)] for _ in range(9)]
     
     for idx, cell in enumerate(cells):
         x1, y1, x2, y2 = cell
         roi = capture_roi(x1, y1, x2, y2, img)
+
+        roi = cv2.blur(roi, (7, 7))
+
+        # Display the blurred cell image for verification
+        #cv2.imshow(f'Blurred Cell {idx+1}', roi)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
         
         number = extract_number_from_image(roi)
         
         row = idx // 9
         col = idx % 9
         
-        if number:
+        if number != 9:
+            verified_number = verify_number_in_image(roi, '9')
+            if verified_number == 9:
+                sudoku_array[row][col] = 9
+                print(f"Cell {row+1}-{col+1}: Number verified as 9")
+            else:
+                sudoku_array[row][col] = number
+                print(f"Cell {row+1}-{col+1}: Number identified - {number}")
+        else:
             sudoku_array[row][col] = number
             print(f"Cell {row+1}-{col+1}: Number identified - {number}")
-        else:
-            sudoku_array[row][col] = 0
-            print(f"Cell {row+1}-{col+1}: No number identified - 0")
-        
-        #Display the ROI for visual verification
-        #cv2.imshow('ROI', roi)
-        #cv2.waitKey(0)
     
     return sudoku_array
