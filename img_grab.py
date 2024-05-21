b1a91d315838ba0360b317963a71810e2c2dafec
Siddharth Panditrao <115323514+SoloSid69@users.noreply.github.com>
2024-05-21 08:15:16 +0530

    Update img_grab.py

--- img_grab.py
+++ img_grab.py
@@ -11,5 +11,8 @@ def screen_grab(x1, y1, x2, y2):
     
     # Convert to grayscale
     gray = cv2.cvtColor(screen_np, cv2.COLOR_RGB2GRAY)
+
+    #Thresholding
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 11, 2)
     
     return gray
