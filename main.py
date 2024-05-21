85224c06b2e2ddd4746b6760e629ed1dcd0872ac
Siddharth Panditrao <115323514+SoloSid69@users.noreply.github.com>
2024-05-21 12:49:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ def main():
     x1, y1 = 293, 299
     x2, y2 = 825, 831
     img = img_grab.screen_grab(x1, y1, x2, y2)
+    #img = cv2.blur(img, (7, 6))
+
     
     # Display the captured image
     cv2.imshow('Captured Image', img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+    
 
     cells = ocr.divide()
     sudoku_array = ocr.process(cells, img)
