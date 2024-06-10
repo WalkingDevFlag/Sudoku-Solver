4bb2103fcafc987d269520df4a821e6a65e35463
Siddharth Panditrao <115323514+WalkingDevFlag@users.noreply.github.com>
2024-06-11 00:29:50 +0530

    Delete Screen Size.py

--- Screen Size.py
+++
@@ -1,7 +0,0 @@
-import pyautogui
-
-# Get the screen width and height
-screen_width, screen_height = pyautogui.size()
-
-print(f"Screen width: {screen_width}")
-print(f"Screen height: {screen_height}")
